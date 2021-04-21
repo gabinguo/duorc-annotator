abbce8e4a5ec0864d5ce1e7c317ec6077b470e6c
gabinguo <gabin.guo@gmail.com>
2021-04-21 20:33:52 +0200

    reformat the code abit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from "react"
+import React, { useState, useEffect, useRef } from "react"
 import './App.css';
 import 'antd/dist/antd.css';
 import { Row, Col, Space, Progress, Button, Spin, message, notification, Modal } from "antd"
@@ -19,19 +19,19 @@ function App() {
   const [passed, setPassed] = useState(0)
   const [failed, setFailed] = useState(0)
   const progressBarRef = useRef(null)
-  
+
 
 
   const req_single_example = (idx) => {
     return axios.get(`/data/${idx}`)
-  } 
+  }
 
   const req_overall_count = () => {
     return axios.get("/overview")
   }
 
   const update_overall_count = (count) => {
-    return axios.post("/overview", JSON.stringify({"count": count}), {
+    return axios.post("/overview", JSON.stringify({ "count": count }), {
       "headers": {
         "Content-Type": "application/json"
       }
@@ -55,7 +55,7 @@ function App() {
   }
 
   useEffect(() => {
-    if (!storageUtil.getTmpKey()){
+    if (!storageUtil.getTmpKey()) {
       const uuid = uuidv4()
       memoryUtil.tmp_key = uuid
       storageUtil.saveTmpKey(memoryUtil)
@@ -63,19 +63,19 @@ function App() {
     memoryUtil.tmp_key = storageUtil.getTmpKey()
     setIsFetching(true)
     req_examples().then(res => {
-      if (res){
+      if (res) {
         const dataIds = res["data"].map(r => r["data_id"])
         const user_ids = res["data"].map(r => r["user_id"])
         const occurence_map = user_ids.reduce((acc, curr) => {
-          if (typeof acc[curr] === "undefined"){
+          if (typeof acc[curr] === "undefined") {
             acc[curr] = 1
-          }else{
+          } else {
             acc[curr] += 1
           }
           return acc
         }, {})
         const leaderboard = []
-        Object.keys(occurence_map).sort(function(a,b){return occurence_map[b]-occurence_map[a]}).forEach(key => {
+        Object.keys(occurence_map).sort(function (a, b) { return occurence_map[b] - occurence_map[a] }).forEach(key => {
           leaderboard.push({
             key,
             score: occurence_map[key]
@@ -83,15 +83,15 @@ function App() {
         })
         setLeaderboard(leaderboard)
         return dataIds
-      } 
+      }
     }).then((dataIds) => {
-      if (dataIds){
-        while(true){
+      if (dataIds) {
+        while (true) {
           var randInt = Math.floor(Math.random() * 60000);
           if (dataIds.indexOf(randInt) === -1) break;
         }
         req_single_example(randInt).then(res => {
-          if (res){
+          if (res) {
             setExample(res["data"])
             setIsFetching(false)
           }
@@ -100,19 +100,19 @@ function App() {
     })
 
     req_overall_count().then(res => {
-      if(res){
+      if (res) {
         setProgressStatus(res["data"]["count"])
       }
     })
 
     req_examples_by_tmp_key(memoryUtil.tmp_key).then(res => {
-      if(res){
+      if (res) {
         let pass = 0
         let fail = 0
         res["data"].forEach(example => {
           const context = example["paragraphs"][0]["context"]
-          const {text, answer_start} = example["paragraphs"][0]["qas"][0]["answers"][0]
-          if (context.substring(answer_start, answer_start + text.length) === text){
+          const { text, answer_start } = example["paragraphs"][0]["qas"][0]["answers"][0]
+          if (context.substring(answer_start, answer_start + text.length) === text) {
             pass += 1
           } else {
             fail += 1
@@ -130,11 +130,11 @@ function App() {
     const selection = window.getSelection()
     let startPos = selection.getRangeAt(0).startOffset
     const selected_text = selection.toString() || ""
-    if (selected_text){
-      if (selected_text.split(" ").length <= 15){
+    if (selected_text) {
+      if (selected_text.split(" ").length <= 15) {
         setAnswerSelected(selected_text.trim())
         setAnswerStartPos(startPos)
-      }else{
+      } else {
         message.warning("Try to limit the length of answers under 15 words.")
       }
     }
@@ -146,7 +146,7 @@ function App() {
     }
     setIsFetching(true)
     req_single_example(Math.floor(Math.random() * 60000)).then(res => {
-      if(res){
+      if (res) {
         setExample(res["data"])
         setIsFetching(false)
         setAnswerSelected("")
@@ -156,8 +156,8 @@ function App() {
 
   const handleSubmit = () => {
     setIsFetching(true)
-    const { question, plot, title, question_id, no_answer, id} = example
-    if (!answerSelected){
+    const { question, plot, title, question_id, no_answer, id } = example
+    if (!answerSelected) {
       message.error("No answer selected.")
       setIsFetching(false)
       return
@@ -182,7 +182,7 @@ function App() {
       ]
     }
     send_single_example(payload).then(res => {
-      if (res){
+      if (res) {
         update_overall_count(progressStatus + 1)
         setProgressStatus(progressStatus + 1)
         notification.success({
@@ -191,13 +191,13 @@ function App() {
           placement: "bottomRight"
         })
         axios.get(`/annotations?user_id=${memoryUtil.tmp_key}`).then(res => {
-          if(res){
+          if (res) {
             let pass = 0
             let fail = 0
             res["data"].forEach(example => {
               const context = example["paragraphs"][0]["context"]
-              const {text, answer_start} = example["paragraphs"][0]["qas"][0]["answers"][0]
-              if (context.substring(answer_start, answer_start + text.length) === text){
+              const { text, answer_start } = example["paragraphs"][0]["qas"][0]["answers"][0]
+              if (context.substring(answer_start, answer_start + text.length) === text) {
                 pass += 1
               } else {
                 fail += 1
@@ -209,7 +209,7 @@ function App() {
           }
         })
         req_single_example(Math.floor(Math.random() * 60000)).then(res_single => {
-          if(res_single){
+          if (res_single) {
             setExample(res_single["data"])
             setIsFetching(false)
             setAnswerSelected("")
@@ -220,21 +220,21 @@ function App() {
         })
         return res
       }
-    }).then(res => {
-      if(res){
+    }).then(pass_res => {
+      if (pass_res) {
         req_examples(res_all => {
-          if (res_all){
+          if (res_all) {
             const user_ids = res_all["data"].map(r => r["user_id"])
             const occurence_map = user_ids.reduce((acc, curr) => {
-              if (typeof acc[curr] === "undefined"){
+              if (typeof acc[curr] === "undefined") {
                 acc[curr] = 1
-              }else{
+              } else {
                 acc[curr] += 1
               }
               return acc
             }, {})
             const leaderboard = []
-            Object.keys(occurence_map).sort(function(a,b){return occurence_map[b]-occurence_map[a]}).forEach(key => {
+            Object.keys(occurence_map).sort(function (a, b) { return occurence_map[b] - occurence_map[a] }).forEach(key => {
               leaderboard.push({
                 key,
                 score: occurence_map[key]
@@ -247,44 +247,17 @@ function App() {
     })
   }
 
-  const leaderboardComponent = (
-    <Row>
-        <div className="annotation-status" style={{textAlign: "center"}}>
-          <h1>Leaderboard</h1>
-            <table style={{width: "100%"}}>
-              <thead>
-                <tr>
-                  <th>Rank</th>
-                  <th>key</th>
-                  <th>total</th>
-                </tr>
-              </thead>
-              <tbody>
-                {leaderboard.map((info, index) => {
-                  return (
-                    <tr>
-                      <td>{index+1}</td>
-                      <td>{info["key"]}</td>
-                      <td style={{color: "green"}}>{info["score"]}</td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </table>
-        </div>
-    </Row>
-  )
 
   return (
     <div className="App">
       <Spin spinning={isFetching}>
-        <Space direction="vertical" size="large" style={{width: "100%"}}>
+        <Space direction="vertical" size="large" style={{ width: "100%" }}>
           <Row>
             <div className="progress-bar" ref={progressBarRef}>
-              <Progress 
-                status="active" 
-                percent={progressStatus/1600 * 100} 
-                format={percent => `${Math.floor(percent/100 * 1600)}/1600`}
+              <Progress
+                status="active"
+                percent={progressStatus / 1600 * 100}
+                format={percent => `${Math.floor(percent / 100 * 1600)}/1600`}
                 strokeColor={{
                   '0%': '#1890FF',
                   '100%': '#81b214',
@@ -310,22 +283,22 @@ function App() {
             </Col>
             <Col flex="3">
               <div className="answers">
-                <div style={{height: "80%", position: "sticky", top: 20}}>
-                  <Row style={{marginRight: 0}}>
+                <div style={{ height: "80%", position: "sticky", top: 20 }}>
+                  <Row style={{ marginRight: 0 }}>
                     <div className="answer-in-dataset">
                       <div className="answer-in-dataset-header">Answer in DuoRC</div>
                       <div>{example["answers"] && example["answers"][0]}</div>
                       <div className="answer-in-dataset-skip-button">
-                        <Button style={{width: "100%"}} type="primary" onClick={() => handleSkip()}>Skip</Button>
+                        <Button style={{ width: "100%" }} type="primary" onClick={() => handleSkip()}>Skip</Button>
                       </div>
                     </div>
                   </Row>
-                  <Row style={{marginRight: 0}}>
+                  <Row style={{ marginRight: 0 }}>
                     <div className="answer-selected">
                       <div className="answer-selected-header">Answer you selected</div>
                       <div>{answerSelected || "\"Select your answer in the plot...\""}</div>
                       <div className="answer-selected-submit-button">
-                        <Button style={{width: "100%"}} type="primary" onClick={() => handleSubmit()}>Submit</Button>
+                        <Button style={{ width: "100%" }} type="primary" onClick={() => handleSubmit()}>Submit</Button>
                       </div>
                     </div>
                   </Row>
@@ -334,47 +307,71 @@ function App() {
             </Col>
           </Row>
 
-          
+
           <Row>
             <div className="annotation-status">
-                <table style={{width: "100%"}}>
-                  <thead>
-                    <tr>
-                      <th>Total</th>
-                      <th>Passed</th>
-                      <th>Failed</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{countAnnotated}</td>
-                      <td style={{color: "green"}}>{passed}</td>
-                      <td style={{color: "red"}}>{failed}</td>
-                    </tr>
-                  </tbody>
-                </table>
+              <table style={{ width: "100%" }}>
+                <thead>
+                  <tr>
+                    <th>Total</th>
+                    <th>Passed</th>
+                    <th>Failed</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{countAnnotated}</td>
+                    <td style={{ color: "green" }}>{passed}</td>
+                    <td style={{ color: "red" }}>{failed}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </Row>
-          <div style={{textAlign: "end"}}>
+          <div style={{ textAlign: "end" }}>
             <h3>Key: {memoryUtil.tmp_key}</h3>
           </div>
         </Space>
       </Spin>
       <footer>
-          <p onClick={() => setShowLeaderboard(!showLeaderboard)}>
-            Thanks for your contribution.
+        <p onClick={() => setShowLeaderboard(!showLeaderboard)}>
+          Thanks for your contribution.
           </p>
       </footer>
-      <Modal 
-        visible={showLeaderboard} 
-        onCancel={() => setShowLeaderboard(false)} 
+      <Modal
+        visible={showLeaderboard}
+        onCancel={() => setShowLeaderboard(false)}
         onOk={() => setShowLeaderboard(false)}
-        okButtonProps={{style: {display: "none"}}}
-        cancelButtonProps={{style: {display: "none"}}}
+        okButtonProps={{ style: { display: "none" } }}
+        cancelButtonProps={{ style: { display: "none" } }}
         closable={false}
         width={800}
       >
-        {leaderboardComponent}
+        <Row>
+          <div className="annotation-status" style={{ textAlign: "center" }}>
+            <h1>Leaderboard</h1>
+            <table style={{ width: "100%" }}>
+              <thead>
+                <tr>
+                  <th>Rank</th>
+                  <th>key</th>
+                  <th>total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {leaderboard.map((info, index) => {
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{info["key"]}</td>
+                      <td style={{ color: "green" }}>{info["score"]}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </Row>
       </Modal>
     </div>
   );
