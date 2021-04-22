c17f8f04ac6859b5f798d519a8818f34777ecfa9
Kunpeng GUO <kunpeng,guo@etu.univ-st-etienne.fr>
2021-04-22 13:27:26 +0200

    add Ali

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ const name_id_map = {
   "c2c412f2-58aa-4b3b-bc16-54b6c552b392": "Dennis",
   "16595f2f-69b4-4262-a52d-b4d86c93fa25": "Guo",
   "417d258e-1614-4098-bf3c-dfbe07f733f4": "Clément",
-  "19a37fb8-61ae-4a66-a8f0-ba3bd684a2b6": "Pierre"
+  "19a37fb8-61ae-4a66-a8f0-ba3bd684a2b6": "Pierre",
+  "ea2c4fc5-eb3b-45d4-8d29-c12bfeb11659": "Ali"
 }
 
 const god_mode = ["16595f2f-69b4-4262-a52d-b4d86c93fa25", "16595f2f-69b4-4262-a52d-b4d86c93fa25"]
