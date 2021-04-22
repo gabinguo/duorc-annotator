22946b42170ea9347a8416f5ee3065c41f1a3e08
Kunpeng GUO <kunpeng,guo@etu.univ-st-etienne.fr>
2021-04-22 14:23:50 +0200

    add Jonathan

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ const name_id_map = {
   "16595f2f-69b4-4262-a52d-b4d86c93fa25": "Guo",
   "417d258e-1614-4098-bf3c-dfbe07f733f4": "Clément",
   "19a37fb8-61ae-4a66-a8f0-ba3bd684a2b6": "Pierre",
-  "ea2c4fc5-eb3b-45d4-8d29-c12bfeb11659": "Ali"
+  "ea2c4fc5-eb3b-45d4-8d29-c12bfeb11659": "Ali",
+  "a5e2d773-4924-4de7-9399-d5e52343461d": "Jonathan"
 }
 
 
